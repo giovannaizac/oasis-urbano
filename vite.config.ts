01d94697b8928e4e117fc9f823f7d10b459c70b4
Giovanna Izac <giovannaizac2@hotmail.com>
2026-09-08 21:25:41 -0300

    Fix base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/oasisUrbano/',
+	base: '/oasis-urbano/',
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
